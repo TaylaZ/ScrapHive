6f4e7adca57ad954c5bc6e0a91e18203f964f5dc
TaylaZ <WZHOU008@e.ntu.edu.sg>
2023-08-10 15:25:30 +0800

    Bottom Appbar UI, Add Posts UI, selecting image

--- lib/screens/default_screen.dart
+++ lib/screens/default_screen.dart
@@ -1,9 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:scraphive/providers/user_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:scraphive/utils/global_variables.dart';
 import '../models/user.dart' as usermodel;
+import 'package:flutter/cupertino.dart';
+import '../utils/colors.dart';
 
 class DefaultScreen extends StatefulWidget {
   const DefaultScreen({Key? key}) : super(key: key);
@@ -14,12 +18,30 @@ class DefaultScreen extends StatefulWidget {
 
 class _DefaultScreenState extends State<DefaultScreen> {
   String username = "";
+  int _page = 0;
+  late PageController pageController;
 
   @override
   void initState() {
     super.initState();
-
     addData();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
+  void navigationTapped(int page) {
+    pageController.jumpToPage(page);
   }
 
   addData() async {
@@ -29,10 +51,59 @@ class _DefaultScreenState extends State<DefaultScreen> {
 
   @override
   Widget build(BuildContext context) {
+
+    
     usermodel.User user = Provider.of<UserProvider>(context).getUser;
     return Scaffold(
-      body: Center(
-        child: Text(user.username),
+      body: PageView(
+        children: homeScreenItems,
+        physics: const NeverScrollableScrollPhysics(),
+        controller: pageController,
+        onPageChanged: onPageChanged,
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              (_page == 0) ? EvaIcons.home : EvaIcons.homeOutline,
+            ),
+            label: 'Home',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                EvaIcons.search,
+              ),
+              label: 'Search',
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(
+                (_page == 2) ? EvaIcons.plusCircle : EvaIcons.plusCircleOutline,
+              ),
+              label: 'Add Post',
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+            icon: Icon(
+              (_page == 3) ? EvaIcons.heart : EvaIcons.heartOutline,
+            ),
+            label: 'Favourites',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              (_page == 4) ? EvaIcons.person : EvaIcons.personOutline,
+            ),
+            label: 'Profile',
+            backgroundColor: primaryColor,
+          ),
+        ],
+        iconSize: 20,
+        activeColor: Colors.amber,
+        inactiveColor: Colors.brown,
+        backgroundColor: Colors.yellow[50],
+        onTap: navigationTapped,
+        currentIndex: _page,
+        height: 50,
       ),
     );
   }
---
+++ lib/utils/global_variables.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:scraphive/screens/add_post_screen.dart';
+
+const List<Widget> homeScreenItems = [
+  Text('1'),
+  Text('2'),
+  AddPostScreen(),
+  Text('4'),
+  Text('5'),
+];
